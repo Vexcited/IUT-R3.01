c7fc68d05744a8bde5d09493e1df64ccc9ffd8aa
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-20 10:55:45 +0200

    feat(TP1): add semantics because why not

--- TP1/src/Views/contact.php
+++ TP1/src/Views/contact.php
@@ -1,5 +1,7 @@
-<h1>Formulaire de contact</h1>
-
-<p>
-  Merci de remplir ce formulaire pour nous contacter.
-</p>
+<main>
+  <h1>Formulaire de contact</h1>
+  
+  <p>
+    Merci de remplir ce formulaire pour nous contacter.
+  </p>
+</main>
