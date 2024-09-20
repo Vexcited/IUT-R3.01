e4f38d08318bcf61f7c70bd1aa9d52a57de3849d
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-20 10:33:34 +0200

    feat(TP1): add icons

--- TP1/src/Views/header.php
+++ TP1/src/Views/header.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <title>La Cosina</title>
 </head>
 <body>
@@ -14,7 +15,7 @@
           class="nav-link"
           href="/?c=home"
         >
-          Accueil
+          <i class="bi bi-house"></i>
         </a>
       </li>
       <li class="nav-item">
@@ -22,7 +23,7 @@
           class="nav-link"
           href="/?c=contact"
         >
-          Contact
+          <i class="bi bi-envelope"></i> Contact
         </a>
       </li>
     </ul>
