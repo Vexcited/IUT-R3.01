952faf4e08b8d0c96144551c11388433ada02084
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-25 09:12:35 +0200

    fix(TP2/db): use env

--- TP2/src/Models/connectDb.php
+++ TP2/src/Models/connectDb.php
@@ -1,9 +1,11 @@
 <?php
 
 try {
-  $host = "db";
-  $dbname = "lacosina";
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 'root', 'rootpwd');
+  $host = getenv("DB_HOST");
+  $dbname = getenv("DB_DATABASE");
+  $username = getenv("DB_USERNAME");
+  $password = getenv("DB_PASSWORD");
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 }
 catch (PDOException $e) {
   echo 'Erreur : ' . $e->getMessage();
