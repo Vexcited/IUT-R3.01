829f5c610bddfef56689a31a6f7d053fd440e31f
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-13 09:29:30 +0100

    feat(TPF2): add design for account creation

--- FINALx2_TP/src/views/login.php
+++ FINALx2_TP/src/views/login.php
@@ -4,12 +4,21 @@ require_once 'views/header.php';
 
 <main class="container mx-auto">
   <form action="/?c=user&a=login" method="POST"
-    class="flex flex-col gap-2 max-w-[300px] bg-gray-200 p-4 mx-auto"
+    class="flex flex-col gap-6 px-4 py-6 mx-auto max-w-[600px]"
   >
-    <input type="email" placeholder="E-Mail" name="email" required>
-    <input type="password" placeholder="Mot de passe" name="password" required>
+    <div class="flex flex-col">
+      <h1 class="text-xl font-bold">Connectez vous</h1>
+      <p>
+        Vous n'avez pas de compte ? <a href="/?c=user&a=d-login" class="dark:text-[#c6a0f6] text-[#8839ef] hover:underline">Créez en un aujourd'hui !</a>
+      </p>
+    </div>
 
-    <button type="submit">
+    <div class="flex flex-col gap-4">
+      <input type="email" class="outline-none px-4 py-2 rounded-2xl bg-[#e6e9ef] dark:bg-[#1e2030] w-full focus:outline-2 focus:dark:outline-[#c6a0f6] focus:outline-[#8839ef]" placeholder="E-Mail" name="email" required>
+      <input type="password" class="outline-none px-4 py-2 rounded-2xl bg-[#e6e9ef] dark:bg-[#1e2030] w-full focus:outline-2 focus:dark:outline-[#c6a0f6] focus:outline-[#8839ef]" placeholder="Mot de passe" name="password" required>
+    </div>
+
+    <button type="submit" class="px-4 py-2 rounded-2xl hover:bg-[#8839EF] bg-[#8839EF]/90 text-[#EFF1F5] dark:hover:bg-[#c6a0f6] dark:bg-[#c6a0f6]/90 dark:text-[#24273a]">
       Se connecter
     </button>
   </form>
